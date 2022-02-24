01775258082516c05057583a6af934b15f7d8ed7
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-02-24 21:52:56 +0530

    Update cf template.cpp

--- cf template.cpp
+++ cf template.cpp
@@ -5,8 +5,7 @@ using namespace std;
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
-template <class T> using ordered_set = tree<T, null_type, less<T>, 
-					rb_tree_tag, tree_order_statistics_node_update>;
+template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define ll long long
 #define ld long double
