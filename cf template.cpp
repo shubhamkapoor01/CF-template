853cea21bb26e339315cfc2107811afb288850c4
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-02-24 21:52:40 +0530

    Add files via upload

---
+++ cf template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <class T> using ordered_set = tree<T, null_type, less<T>, 
+					rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+#define endl "\n"
+
+
+int main () {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	ll tc;
+	cin >> tc;
+	while (tc --) {
+
+
+	}
+	return 0;
+}
