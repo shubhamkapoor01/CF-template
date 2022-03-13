fffef424c81dc6e3a056af51c9e6c23a9478f228
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-03-13 16:51:27 +0530

    Update cf template.cpp

--- cf template.cpp
+++ cf template.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
 
-template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
+#define eb emplace_back
 #define ll long long
 #define ld long double
 #define pb push_back
