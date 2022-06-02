31ed124fa0981bdbad338e02a847270c77c06752
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-02 15:17:38 +0530

    Update cf template.cpp

--- cf template.cpp
+++ cf template.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
 
-template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, 
+// 				rb_tree_tag, tree_order_statistics_node_update>;
 
 #define eb emplace_back
 #define ll long long
@@ -16,6 +17,22 @@ template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, 
 #define ss second
 #define endl "\n"
 
+
+ll power(ll x, ll n) {
+	if (n == 0) return 1;
+	if (n == 1) return x;
+	if (n % 2) return x * power(x, n - 1);
+	ll t = power(x, n / 2);
+	return t * t;
+}
+
+ll powerMod(ll x, ll n, ll m) {
+	if (n == 0) return 1 % m;
+	if (n == 1) return x % m;
+	if (n % 2) return (x % m * power(x, n - 1) % m) % m;
+	ll t = power(x, n / 2);
+	return (t % m * t % m) % m;
+}
 
 int main () {
 	ios::sync_with_stdio(false);
