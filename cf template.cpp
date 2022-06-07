334af40278148a42b64c98ce70f055d7d1be68b5
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-07 14:48:37 +0530

    Update cf template.cpp

--- cf template.cpp
+++ cf template.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // template <class T> using ordered_set = tree<T, null_type, less<T>, 
 // 				rb_tree_tag, tree_order_statistics_node_update>;
 
+#define MOD 1000000007
 #define eb emplace_back
 #define ll long long
 #define ld long double
