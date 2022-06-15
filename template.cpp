9df9d3a2c8b894a5ac19757c1e4f21737c2d2beb
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-15 14:58:09 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,12 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// #include <ext/pb_ds/assoc_container.hpp>
-// #include <ext/pb_ds/tree_policy.hpp>
-// using namespace __gnu_pbds;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 
-// template <class T> using ordered_set = tree<T, null_type, less<T>, 
-// 				rb_tree_tag, tree_order_statistics_node_update>;
+template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// to make unordered_map unhackable
+// use it as unordered_map<int,int,custom_hash> mp;
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        /* http://xorshift.di.unimi.it/splitmix64.c */
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
 
 #define MOD 1000000007
 #define eb emplace_back
