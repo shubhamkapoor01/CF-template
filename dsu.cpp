591f8acf2757dd43420b79ad4cb0da71756a593f
shubhamkapoor01 <66124414+shubhamkapoor01@users.noreply.github.com>
2022-06-17 17:46:31 +0530

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,10 +1,26 @@
+// use it as unordered_map<int,int,custom_hash> mp;
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        /* http://xorshift.di.unimi.it/splitmix64.c */
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
 class DSU {
 private:
-    unordered_map<ll, ll> parent;
-    unordered_map<ll, ll> rank;
+    unordered_map<ll, ll, custom_hash> parent;
+    unordered_map<ll, ll, custom_hash> rank;
     
 public:
-    DSU (ll node) {
+    void makeNode (ll node) {
         if (parent.find(node) != parent.end()) {
             return;
         } else {
